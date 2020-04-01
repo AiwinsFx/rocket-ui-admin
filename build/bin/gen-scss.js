e520d608adefc2a513518df022044dd3f273f877
lihongbo <1301465186@qq.com>
2020-04-01 15:42:20 +0800

    lhb

--- build/bin/gen-scss.js
+++ build/bin/gen-scss.js
@@ -4,9 +4,15 @@ var fs = require('fs');
 var path = require('path');
 const fileSave = require('file-save');
 // console.log('当前文件的路径'.rainbow,);
-var moduleScss = fs.readFileSync(path.resolve(__dirname,'../../packages/theme-chalk/src/common/module.scss'),'utf8',function(err,data){
+var modulePath = path.resolve(__dirname,'../../packages/theme-chalk/src/common/module.scss');
+var moduleScss = fs.readFileSync(modulePath,'utf8',function(err,data){
   console.log('读取的内容',data);
 })
-// fileSave()
+var variablesPath = path.resolve(__dirname,'../../packages/theme-chalk/src/common/variables.scss');
+var variablesScss = fs.readFileSync(variablesPath,'utf8',function(err,data){
+  console.log('读取的内容',data);
+})
+fileSave(modulePath).write(moduleScss.replace(/\/\/.*$/mg, ''),'utf8').end('\n');
+fileSave(variablesPath).write(variablesScss.replace(/\/\/.*$/mg, ''),'utf8').end('\n');
 console.log('输出当前读取的文件'.rainbow,`${moduleScss}`);
 console.log( '输出当前读取的文件moduleScss'.rainbow,moduleScss.replace(/\/\/.*$/mg, '') );
