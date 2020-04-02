c8a5a0f78ec4f69cf27f88a88c5fce933e541427
lihongbo <1301465186@qq.com>
2020-04-02 15:19:55 +0800

    lhb

--- examples/play/index.vue
+++ examples/play/index.vue
@@ -2,6 +2,10 @@
   <div style="margin: 20px;">
     <el-input v-model="input" placeholder="请输入内容"></el-input>
     <el-tag class="tags" type="success">标签二</el-tag>
+    <el-alert
+      title="成功提示的文案"
+      type="success">
+    </el-alert>
   </div>
 </template>
 
