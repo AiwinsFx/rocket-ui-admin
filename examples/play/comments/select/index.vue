da1e51bcc40d598bd4ac23cd226b9476c558305f
lihongbo <1301465186@qq.com>
2020-04-17 10:05:30 +0800

    lhb

---
+++ examples/play/comments/select/index.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <el-select v-model="value" placeholder="请选择">
+      <el-option
+        v-for="item in options"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value"
+      >
+      </el-option>
+    </el-select>
+    <div style="margin-top: 15px;">
+      <el-input
+        placeholder="请输入内容"
+        v-model="input3"
+        class="input-with-select"
+      >
+        <el-select v-model="select" slot="prepend" placeholder="请选择">
+          <el-option label="餐厅名" value="1"></el-option>
+          <el-option label="订单号" value="2"></el-option>
+          <el-option label="用户电话" value="3"></el-option>
+        </el-select>
+        <el-button slot="append" icon="el-icon-search"></el-button>
+      </el-input>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      input3: '',
+      select: '',
+      options: [
+        {
+          value: '选项1',
+          label: '黄金糕',
+        },
+        {
+          value: '选项2',
+          label: '双皮奶',
+        },
+        {
+          value: '选项3',
+          label: '蚵仔煎',
+        },
+        {
+          value: '选项4',
+          label: '龙须面',
+        },
+        {
+          value: '选项5',
+          label: '北京烤鸭',
+        },
+      ],
+      value: '',
+    }
+  },
+  methods:{}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/index.js
+++ src/index.js
@@ -200,7 +200,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 }
 
 export default {
-  version: '1.0.14',
+  version: '1.0.15',
   locale: locale.use,
   i18n: locale.i18n,
   install,
