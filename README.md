6b3354393362fb82bf7fdb180ca2180bf990f753
hywz <756771557@qq.com>
2020-08-03 18:14:34 +0800

    更新步骤

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # @aiwins/admin-ui
-基于Vue的AdminUI框架
+## 基于Vue的AdminUI框架https://juejin.im/post/6844903769365282829#heading-3
+npm version patch
+npm run dist
+npm publish
--- examples/play/comments/table/index.vue
+++ examples/play/comments/table/index.vue
@@ -82,11 +82,11 @@ export default {
           name: '王小虎',
           address: '上海市普陀区金沙江路 1518 弄',
         },
-        {
-          date: '2016-05-07',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄',
-        },
+        // {
+        //   date: '2016-05-07',
+        //   name: '王小虎',
+        //   address: '上海市普陀区金沙江路 1518 弄',
+        // },
       ],
       multipleSelection: [],
     }
